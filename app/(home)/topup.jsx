46c4c2819aa0c49520d0b7612230ed904777286d
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2024-12-16 07:39:47 +0700

    feat: create topup page

--- app/(home)/topup.jsx
+++ app/(home)/topup.jsx
@@ -1,13 +1,56 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
 import Input from "../../components/Input";
+import Amount from "../../components/Amount";
 import Button from "../../components/Button";
+import Dropdown from "../../components/Dropdown";
 
-function Topup() {
+export default function Topup() {
+  const [paymentMethod, setPaymentMethod] = useState(null);
+
   return (
-    <View>
-      <Input text="Notes" />
-      <Button text="Top Up" />
+    <View style={styles.container}>
+      <View style={styles.spacer} />
+      <Amount marginBottom={24} style={styles.component} />
+      <Dropdown
+        options={["BYOND Pay", "E-Wallet", "BSI Mobile"]}
+        placeholder="Select Payment Method"
+        onSelect={(option) => setPaymentMethod(option)}
+        style={styles.component}
+      />
+      <Input text={"Notes"} style={styles.component} />
+      <View style={styles.buttonContainer}>
+        <Button text={"Top Up"} />
+      </View>
     </View>
   );
 }
-export default Topup;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFBFD",
+    paddingTop: 20,
+  },
+  spacer: {
+    // flex: 0.1, // Optional: Space at the top
+  },
+  component: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 20,
+    padding: 15,
+  },
+});
